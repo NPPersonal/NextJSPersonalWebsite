7ae55ec327b5ad2173fd2065d202d42e53c74f89
Nelson <tomneo2004@gmail.com>
2024-10-15 17:38:33 +0800

    feat: add metadata for message me page
    - Add metadata for message me page

--- src/app/[locale]/contact/message-me/laytout.tsx
+++ src/app/[locale]/contact/message-me/laytout.tsx
@@ -1,5 +1,18 @@
+import { routing } from "@/i18n/routing";
 import { NextJSLayoutProps } from "@/types/page-props";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export async function generateMetadata() {
+  const t = await getTranslations("MessageMe");
+  return {
+    title: t("metadata_title"),
+    description: t("metadata_description"),
+  };
+}
 
 export default function MessageMeLayout({
   params: { locale },
